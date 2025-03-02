521540fde5fc1a0fcbca56f97a662c37871d779f
Ivan Pavlov <ippavlov@avito.ru>
2025-03-02 13:57:00 +0300

    add basic implementation and tests

--- internal/service/parser/parse.go
+++ internal/service/parser/parse.go
@@ -6,13 +6,42 @@ func (s *Service) Parse(ctx context.Context, regex string) bool {
 	r := []rune(regex)
 
 	var i int
+	var brCount, grCount int
+	var maxNum rune
 
 	for i < len(r) {
 		_, ok := s.allowedChars[r[i]]
 		if !ok {
 			return false
 		}
+
+		if i < len(r)-3 && r[i] == '(' && r[i+1] == '?' && s.IsDigit(r[i+2]) && r[i+3] == ')' {
+			if r[i+2] > maxNum {
+				maxNum = r[i+2]
+			}
+			i += 4
+		} else if i < len(r)-3 && r[i] == '(' && r[i+1] == '\\' && s.IsDigit(r[i+2]) && r[i+3] == ')' {
+			if int(r[i+2]-'0') > grCount {
+				return false
+			}
+			i += 4
+		} else if i < len(r)-1 && r[i] == '\\' && s.IsDigit(r[i+1]) {
+			if int(r[i+1]-'0') > grCount {
+				return false
+			}
+			i += 2
+		} else if r[i] == '(' {
+			brCount++
+		} else if r[i] == ')' {
+			brCount--
+			grCount++
+		}
+
 		i++
+	}
+
+	if brCount != 0 || grCount > 9 || int(maxNum-'0') > grCount {
+		return false
 	}
 
 	return true
--- internal/service/parser/parse_test.go
+++ internal/service/parser/parse_test.go
@@ -13,9 +13,13 @@ func TestService_Parse(t *testing.T) {
 		in   string
 		want bool
 	}{
-		{"valid", "(aa|bb)(?1)", true},
-		{"valid", "(aa|bb)\\1", true},
-		{"invalid", "INVALID INPUT", false},
+		{"valid/ссылка_на_выражение", "(aa|bb)(?1)", true},
+		{"valid/ссылка_на_строку", "(aa|bb)\\1", true},
+		{"invalid/некорректный_ввод", "INVALID INPUT", false},
+		{"invalid/ссылка_на_несуществующую_строку", "(aa|bb)\\2", false},
+		{"invalid/ссылка_на_неинициализированную_строку", "(\\1)(a|b)", false},
+		{"invalid/незакрытые_скобки", "((aa)abaa", false},
+		{"invalid/неоткрытые_скобки", "aa)aaab", false},
 	}
 
 	for _, tt := range tests {
